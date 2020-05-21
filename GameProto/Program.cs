fa709201948ba91871a10d570cc13f2ed1072873
Tegri <Gopkina@prg-it.ru>
2020-05-21 21:21:09 +0500

    first C# programm

---
+++ GameProto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameProto
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            SumGame game1 = new SumGame();
+            Console.Write("Сложить 2 числа?: Y/N ");
+            string answer = Console.ReadLine();
+            game1.Test(answer);
+        }
+    }
+}
---
+++ GameProto/SumGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProto
+{
+    class SumGame
+    {
+        public void Test(string answer)
+        {
+            if (answer == "Y" || answer == "y")
+            {
+                Console.Write("Введи 1 число:");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введи 2 число:");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"{a}+{b} = {a + b}");
+
+                Console.WriteLine("Ещу раз? : Y/N");
+                Test(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Ну как хочешь");
+
+            }
+        }
+    }
+}
