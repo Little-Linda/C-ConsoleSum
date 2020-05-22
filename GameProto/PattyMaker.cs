ff89ef5a6cb507f49770a7a20541a20067fcab7c
Tegri <Gopkina@prg-it.ru>
2020-05-22 10:54:43 +0500

    array with random length of with no uniq numbers

---
+++ GameProto/PattyMaker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace GameProto
+{
+    class Order
+    {
+        // формируем рандомное число что бы задать его как длинну массива
+        static Random random = new Random();
+        static int randomNumber = random.Next(3, 7);
+        //пока пустой массив для "заказа"
+
+        public int[] arrOrder = new int[randomNumber];
+
+        //список для выполнения "заказа"
+        public List<int> list = new List<int>();
+        
+                public void GenerateRandom()
+                {
+                    int Min = 2;
+                    int Max = 7;
+                    Random randNum = new Random();
+                    int test = randNum.Next(1, 7);
+                    for (int i = 0; i < arrOrder.Length; i++)
+                    {
+                        
+
+                        if (!arrOrder.Contains(test))
+                        {
+                            Console.WriteLine(test);
+                            Console.WriteLine("=================");
+                            //добавляем в пустой массив рандомное значение от 1 до 7
+                            arrOrder[i] = test;
+                        } 
+                    }
+                    //return arrOrder;
+                    Console.WriteLine(string.Join(",", arrOrder));
+        }
+
+
+     
+
+    }
+    class PattyMaker
+    {
+
+    }
+}
--- GameProto/Program.cs
+++ GameProto/Program.cs
@@ -6,11 +6,15 @@ namespace GameProto
     class Program
     {
         static void Main(string[] args)
-        { 
-            SumGame game1 = new SumGame();
-            Console.Write("Сложить 2 числа?: Y/N ");
-            string answer = Console.ReadLine();
-            game1.Test(answer);
+        {
+            // SumGame game1 = new SumGame();
+            //Console.Write("Сложить 2 числа?: Y/N ");
+            //string answer = Console.ReadLine();
+            //game1.Test(answer);
+
+            Order order = new Order();
+            order.GenerateRandom();
+            
         }
     }
 }
