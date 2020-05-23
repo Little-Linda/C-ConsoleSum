d0cadd8c7e67ee4befb34dbda836cc0cd8ef76ce
Tegri <Gopkina@prg-it.ru>
2020-05-23 21:09:11 +0500

    create list with random num

--- GameProto/PattyMaker.cs
+++ GameProto/PattyMaker.cs
@@ -7,45 +7,114 @@ namespace GameProto
 {
     class Order
     {
-        // формируем рандомное число что бы задать его как длинну массива
-        static Random random = new Random();
-        static int randomNumber = random.Next(3, 8);
-        //пока пустой массив для "заказа" 
+        // формируем рандомное число что бы задать его как длинну массива       
+        // static Random random = new Random();
+        // static int randomNumber = random.Next(3, 4);
 
-        public int[] arrOrder = new int[randomNumber];
-
-        //список для выполнения "заказа"
+        //список для выполнения "заказа" 
         public List<int> list = new List<int>();
-        
-                public void GenerateRandom()
+
+        public void GenerateRandom()
+        {
+
+            Random random = new Random();
+            int randomNumber = random.Next(3, 7);
+            list.Clear();
+            Console.WriteLine(string.Join(",", list));
+
+            int Min = 2;
+            int Max = 7;
+            Random randNum = new Random();
+            list.Add(1);
+            for (int i = 0; i < randomNumber; i++)
+            {
+                int randomN = randNum.Next(2, 6);
+
+                if (list.Exists((x) => x == randomN))
                 {
-                    int Min = 2;
-                    int Max = 7;
-                    Random randNum = new Random();
+                    randomN = randNum.Next(2, 6);
+                }
 
-                    for (int i = 0; i < arrOrder.Length; i++)
-                    {
-                        arrOrder[0] = 1;
-                        int test = randNum.Next(2, 6);
+                list.Add(randomN);
+            }
+            list.Add(7);
 
-                        if (arrOrder.Contains(test))                        
-                        { 
-                            test = randNum.Next(2, 6);
-                        }
+            Console.WriteLine(string.Join(",", list));
 
-                        arrOrder[i] = test;
-                        arrOrder[arrOrder.Length - 1] = 7;
-                    }
-                    //return arrOrder;
-                    Console.WriteLine(string.Join(",", arrOrder));
         }
-
-
-     
 
     }
     class PattyMaker
     {
+        List<int> ingredients = new List<int>();
 
+        public void Add(int ingredient)
+        {
+            ingredients.Add(ingredient);
+            string sandvList = string.Join(",", ingredients.ToArray());
+            Console.WriteLine(sandvList);
+
+            /*if (ingredients.Count <= len){ }*/
+        }
+        public void ClearIngredients()
+        {
+            ingredients.Clear();
+        }
+
+    }
+    class Game
+    {
+        public void Cooking(string answer)
+        {
+
+            if (answer == "Y" || answer == "y")
+            {
+                ConsoleColor color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkGreen; // выводим список команд зеленым цветом
+                Console.WriteLine("1.Булочка  \t 2. Котлетка  \t 3. Салатик");
+                Console.WriteLine("4. Помидорка \t 5. Огурчики \t 6. Сырочек \t 7. Булочка с кунжутом");
+                Console.WriteLine("Введите номер ингредиента:");
+                Console.ForegroundColor = color;
+
+                Order order = new Order();
+                PattyMaker pattyMaker = new PattyMaker();
+                Console.WriteLine("=====заказ котика Тома=====");
+                order.GenerateRandom();
+                Console.WriteLine("===========================");
+
+                for (int i = 0; i <= order.list.Count - 1; i++)
+                {
+
+                    string name = Console.ReadLine();
+                    int Num;
+                    bool isNum = int.TryParse(name.ToString(), out Num);
+                    if (isNum)
+                    {
+                        int test = Convert.ToInt32(name);
+                        if (test == 7)
+                        {
+                            Console.WriteLine("You make a sandvich! Yam!");
+                            Console.WriteLine("Сделать еще?!: Y/N");
+                            Cooking(Console.ReadLine());
+                        }
+                        else
+                        {
+                            pattyMaker.Add(test);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("это не цифра");
+                    }
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("жаль, попрошу кого-нибудь другого");
+            }
+            Console.Read();
+        }
     }
 }
--- GameProto/Program.cs
+++ GameProto/Program.cs
@@ -2,19 +2,63 @@
 using System.Collections.Generic;
 
 namespace GameProto
-{   
+{
     class Program
     {
+
+        public void Cooking(string answer)
+        {
+            if (answer == "Y" || answer == "y")
+            {
+                Order order = new Order();
+                PattyMaker pattyMaker = new PattyMaker();
+                order.GenerateRandom();
+
+                for (int i = 0; i <= order.list.Count - 1; i++)
+                {
+                    Console.WriteLine("==========1============");
+                    int name = Convert.ToInt32(Console.ReadLine());
+
+                    if (name == 7)
+                    {
+                        Console.WriteLine("You make a sandvich! Yam!");
+                        Console.WriteLine("Сделать еще?!: Y/N");
+                        Cooking(Console.ReadLine());
+                    }
+                    else
+                    {
+                        pattyMaker.Add(name);
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("жаль, попрошу кого-нибудь другого");
+            }
+        }
+
         static void Main(string[] args)
         {
-            // SumGame game1 = new SumGame();
+            //SumGame game1 = new SumGame();
             //Console.Write("Сложить 2 числа?: Y/N ");
             //string answer = Console.ReadLine();
-            //game1.Test(answer);
+            //game1.Test(answer); 
 
-            Order order = new Order();
-            order.GenerateRandom();
-            
+            ConsoleColor color2 = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Magenta; // выводим список команд зеленым цветом
+            Console.WriteLine("Здравствуйте, меня зовут котик Том и я очень хочу кушать, \t");
+            Console.WriteLine("но у меня лапки и я не умею готовить,");
+            Console.WriteLine("помогите мне пожалуйста, приготовьте мне сендвич \t");
+            Console.WriteLine("я выведу на экран цифры, каждая соответсвует конкретному ингредиенту \t");
+            Console.ForegroundColor = color2;
+     
+            Game game = new Game();
+            Console.Write("Накормишь?: Y/N ");
+            string answer = Console.ReadLine();
+            game.Cooking(answer);
+             
+
         }
     }
 }
